04b117938f3b9c23a5f7fba076ef7bab173440ae
Daniel Supernault <danielsupernault@gmail.com>
2018-08-18 00:23:17 -0600

    wip

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,11 +4,11 @@ namespace Dansup\ActivityPub;
 
 class ExampleTest extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * Test that true does in fact equal true
-     */
-    public function testTrueIsTrue()
-    {
-        $this->assertTrue(true);
-    }
+	/**
+	* Test that true does in fact equal true
+	*/
+	public function testTrueIsTrue()
+	{
+		$this->assertTrue(true);
+	}
 }
